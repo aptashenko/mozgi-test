06801b14dd0ad8efa1777b4f7bf5b3734968abac
Artem <aptashenko2019@gmail.com>
2022-06-23 13:03:05 +0300

    localization buttons

--- mozgi/src/App.vue
+++ mozgi/src/App.vue
@@ -1,5 +1,13 @@
 <template>
-    <mainPage />
+  <mainPage />
+  <div class="normalText">
+    <p class="backText">{{backgroundText}}</p>
+    <p class="backText">{{ backgroundText }}</p>
+  </div>
+  <div class="reversedText">
+    <p class="backText backText-reversed">{{ reversedText }}</p>
+    <p class="backText backText-reversed">{{ reversedText }}</p>
+  </div>
 </template>
 
 <script>
@@ -11,13 +19,28 @@ export default {
   name: 'App',
   components: {
     mainPage,
-  }
+  },
+  data() {
+    return {
+      backgroundText: 'FULL-CYCLE EVENT AGENCY',
+    }
+  },
+  computed: {
+    reversedText() {
+      return this.backgroundText.split('').reverse().join('');
+    },
+  },
 }
 </script>
 
 <style lang="scss">
 html, #app {
   height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+#app {
+  overflow: hidden;
 }
 body {
   height: 100%;
@@ -25,9 +48,62 @@ body {
   font-family: 'GrtskGiga';
   color: #373737;
 }
-h1, h2, h3, h4, ul, p {
+h1, h2, h3, h4, ul, p, a {
+  text-decoration: none;
+  color: #363636;
   margin: 0;
   padding: 0;
+}
+
+.normalText, .reversedText {
+  display: flex;
+  position: fixed;
+  white-space: nowrap;
+  width: 200%;
+}
+
+.normalText {
+  bottom: 0;
+  left: -351px;
+  animation: normalText 10s linear infinite;
+  transform: rotateZ(300deg);
+  pointer-events: none;
+}
+
+.reversedText {
+  top: 0;
+  right: -358px;
+  animation: reversedText 10s linear infinite;
+  transform: rotateZ(120deg);
+  pointer-events: none;
+}
+
+.backText {
+  display: inline;
+  font-size: 28px;
+  line-height: 1.1;
+  text-align: center;
+  text-transform: uppercase;
+  color: #bebebe;
+  opacity: 0.1;
+  text-shadow: 0px 1px 0 rgb(55, 55, 55), 0px -1px 0 rgb(55, 55, 55), 1px 0px 0 rgb(55, 55, 55), -1px 0px 0 rgb(55, 55, 55), 1px 1px 0 rgb(55, 55, 55), 1px -1px 0 rgb(55, 55, 55), -1px 1px 0 rgb(55, 55, 55), -1px -1px 0 rgb(55, 55, 55);
+}
+
+@keyframes normalText {
+  0% {
+    transform: rotateZ(300deg) translateX(330px);
+  }
+  100% {
+    transform: rotateZ(300deg) translateX(-150px);
+  }
+}
+@keyframes reversedText {
+  0% {
+    transform: rotateZ(120deg) translateX(360px);
+  }
+  100% {
+    transform: rotateZ(120deg) translateX(-170px);
+  }
 }
 
 </style>
--- mozgi/src/components/mainPage.vue
+++ mozgi/src/components/mainPage.vue
@@ -1,10 +1,15 @@
 <template>
     <div class="wrapper">
         <header class="header">
-            <div class="header__localization">
+            <div class="header__localization" @mouseenter="langShowOpacity()">
                 <img class="header__tongue" src="../assets/images/tongue.png" alt="">
+                <ul class="header__language" v-if="langShow">
+                    <li class="header__language--item" ref="langOption" v-for="lang of langs" :key="lang"><a href="#"
+                            class="header__language--link" @click="langHideOpacity()">{{lang}}</a>
+                    </li>
+                </ul>
             </div>
-            <img class="header__logo" src="../assets/images/logo.svg" alt="">
+            <img src="../assets/images/logo.svg" class="header__logo" alt="">
             <img class="header__burger" src="../assets/images/burger.svg" alt="" @click="mobileMenu = !mobileMenu">
             <div class="mobileMenu" v-if="mobileMenu">
                 <nav class="mobileMenu__navigation">
@@ -23,20 +28,20 @@
             </div>
         </header>
         <main class="hero">
-            <p class="hero__movingText">FULL-CYCLE EVENT AGENCY FULL-CYCLE EVENT AGENCY</p>
-            <h2 class="hero__movingText hero__movingText--invert">{{ reversedText }}</h2>
-            <div class="hero__circle"></div>
-            <h1 class="hero__title">FULL-CYCLE EVENT AGENCY</h1>
-            <div class="hero__circular">
-                <svg viewBox="0 0 100 100">
-                    <path d="M 0,50 a 50,50 0 1,1 0,1 z" id="circle" />
-                    <circle r="6" cx="50" cy="50" fill="black" />
-                    <text class="hero__circular--text">
-                        <textPath xlink:href="#circle">
-                            showreel showreel showreel
-                        </textPath>
-                    </text>
-                </svg>
+            <div class="hero__background">
+                <div class="hero__backdrop"></div>
+                <div class="hero__circular">
+                    <svg viewBox="0 0 100 100">
+                        <path d="M 0,50 a 50,50 0 1,1 0,1 z" id="circle" />
+                        <circle r="6" cx="50" cy="50" fill="black" />
+                        <text class="hero__circular--text">
+                            <textPath xlink:href="#circle">
+                                showreel showreel showreel
+                            </textPath>
+                        </text>
+                    </svg>
+                </div>
+                <h2 class="hero__title" ref="title">FULL-CYCLE EVENT AGENCY</h2>
             </div>
         </main>
     </div>
@@ -47,34 +52,91 @@ export default {
     name: 'mainPage',
     data() {
         return {
+            langs: ['UA', 'RU'],
             mobileMenu: false,
-            backgroundText: 'FULL-CYCLE EVENT AGENCY FULL-CYCLE EVENT AGENCY',
+            langShow: false,
         }
     },
-    computed: {
-        reversedText() {
-            return this.backgroundText.split('').reverse().join('');
-        }
+    methods: {
+        langShowOpacity() {
+            this.langShow = true;
+            setTimeout(() => {
+                if (this.langShow) {
+                    let langIdx = -1;
+                    let id = setInterval(() => {
+                        if (langIdx <= this.$refs.langOption.length-2) {
+                            langIdx += 1;
+                            this.$refs.langOption[langIdx].style.opacity = 1;
+                        } else {
+                            clearInterval(id)
+                        }
+                        }, 100)
+                }
+            },500)
+        },
+        langHideOpacity() {
+            setTimeout(() => {
+                if (this.langShow) {
+                    let langIdx = this.$refs.langOption.length;
+                    let id = setInterval(() => {
+                        if (langIdx > 0) {
+                            langIdx -= 1;
+                            this.$refs.langOption[langIdx].style.opacity = 0;
+                        } else {
+                            clearInterval(id);
+                        }
+                    }, 100)
+                }
+            },500)
+        },
     },
-
 }
 </script>
 
 <style lang="scss">
-
-
-
-
-
-
 .wrapper {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    z-index: 1;
 }
 .header {
     display: flex;
     position: relative;
     justify-content: space-between;
+    align-items: center;
     width: 100%;
     padding: 0 15px;
+    &__localization {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-left: 15px;
+    }
+    &__language {
+        position: absolute;
+        left: 70px;
+        list-style: none;
+        display: flex;
+        &--link {
+            color: #363636;
+            font-size: 12px;
+        }
+        &--item {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 34px;
+            height: 34px;
+            border: 1px solid #363636;
+            border-radius: 50%;
+            opacity: 0;
+            transition: opacity 250ms linear;
+            &:not(:last-child) {
+                margin-right: 5px;
+            }
+        }
+    }
 }
 
 .mobileMenu {
@@ -93,78 +155,77 @@ export default {
         list-style: none;
     }
     &__link {
+        position: relative;
         text-decoration: none;
         color: inherit;
+        &:hover, &:focus {
+            color: #fff;
+            &::after {
+                content: '';
+                position: absolute;
+                width: 0%;
+                height: 5px;
+                top: 100%;
+                left: 0;
+                border-radius: 12px;
+                background: #fff;
+                animation: linkBorder 250ms linear forwards;
+            }
+        }
     }
 }
 
 .hero {
+    display: flex;
+    flex-grow: 1;
     padding: 0 15px;
-    &__movingText {
-        position: absolute;
-        top: 360px;
-        left: 370px;
-        width: max-content;
-        font-size: 28px;
-        line-height: 1.1;
-        text-transform: uppercase;
-        opacity: 0.1;
-        color: #bebebe;
-        text-shadow: 1px 0 0 #000, -1px 0 0 #000, 0 1px 0 #000, 0 -1px 0 #000, 1px 1px #000, -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000;
-        transform: translate(-50%, -50%)
-    }
+    justify-content: center;
+    align-items: center;
     &__background {
         position: relative;
-    }
-    &__circle {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        background: #FFCB46;
-        filter: blur(26px);
-        height: 294px;
         width: 294px;
+        height: 294px;
+    }
+    &__backdrop {
+        width: 100%;
+        height: 100%;
+        filter: blur(20px);
         border-radius: 50%;
-        transform: translate(-50%, -50%);
+        background: #ffcb46;
     }
     &__title {
         position: absolute;
         top: 50%;
         left: 50%;
-        font-style: normal;
-        font-weight: 700;
-        font-size: 34px;
-        line-height: 1.1;
-        text-align: center;
-        text-transform: uppercase;
         transform: translate(-50%, -50%);
-        width: 333px;
+        width: fit-content;
+        text-align: center;
     }
-        &__circular {
-            position: absolute;
-            bottom: 25%;
-            right: 22%;
-            width: 90px;
-            height: 90px;
-    
-            & path {
-                fill: none;
-            }
-    
-            & svg {
-                display: block;
-                overflow: visible;
-            }
-    
-            &--text {
-                fill: #fff;
-                font-size: 12px;
-            }
-    
-            &:hover {
-                animation: rotate 10s infinite linear;
-            }
+    &__circular {
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        width: 90px;
+        height: 90px;
+
+        & path {
+            fill: none;
         }
+
+        & svg {
+            display: block;
+            overflow: visible;
+        }
+
+        &--text {
+            fill: #fff;
+            font-size: 12px;
+        }
+
+        &:hover {
+            animation: rotate 10s infinite linear;
+        }
+    }
 }
 
 @keyframes rotate {
@@ -174,6 +235,14 @@ export default {
 
     100% {
         transform: rotate(-360deg);
+    }
+}
+@keyframes linkBorder {
+    0% {
+        width: 0%;
+    }
+    100% {
+        width: 100%;
     }
 }
 </style>
